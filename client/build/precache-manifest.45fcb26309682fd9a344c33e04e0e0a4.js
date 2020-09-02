36c0f770ed5baf5cc0833d7eca2264110033fc85
Claudinei Bido <bido.c@live.com>
2020-09-02 15:50:33 +0200

    fix

---
+++ client/build/precache-manifest.45fcb26309682fd9a344c33e04e0e0a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a14b37f49c0ccab0a94e4058275dd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e335cafe16e87b3e8c01",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "7e658e36180fc5ce59cc",
+    "url": "/static/css/main.08f6fdad.chunk.css"
+  },
+  {
+    "revision": "e335cafe16e87b3e8c01",
+    "url": "/static/js/2.1a189a92.chunk.js"
+  },
+  {
+    "revision": "3b8322a1623d598370d0afa99de125e0",
+    "url": "/static/js/2.1a189a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e658e36180fc5ce59cc",
+    "url": "/static/js/main.dc569359.chunk.js"
+  },
+  {
+    "revision": "348bd0c3593a6265ed68",
+    "url": "/static/js/runtime-main.089447d4.js"
+  }
+]);
